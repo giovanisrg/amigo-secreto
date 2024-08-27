36c30c6c1d6ecd392299615b0a37b0c0c8f8fd4b
Giovani Silva <giovanibrolyy2016@gmail.com>
2024-08-27 14:38:23 -0300

    id adicionado a linha 35 index, verificação de quantidade minima para sortear, validação se o botão sortear ja foi clicado

--- js/app.js
+++ js/app.js
@@ -1,15 +1,24 @@
 let listaAmigosArray = []
+let sorteioRealizado = false
 
 function adicionar() {
     let nomeAmigo = document.getElementById('nome-amigo')
+
     if (nomeAmigo.value == '') {
        return alert('Digite um nome para adicionar a lista')
     } else {
 
     }
+
     let listaAmigos = document.getElementById('lista-amigos')
-    
-    listaAmigosArray.push(nomeAmigo.value)
+
+    if (listaAmigosArray.includes(nomeAmigo.value)) {
+        alert(`Nome "${nomeAmigo.value}" ja possui na lista, por favor, digite o nome e sobrenome referente a pessoa`)
+        nomeAmigo.value = ''
+        return
+    } else {
+        listaAmigosArray.push(nomeAmigo.value)
+     }
 
     if (listaAmigos.textContent == '') {
         listaAmigos.textContent = nomeAmigo.value
@@ -21,6 +30,13 @@ function adicionar() {
 }
 
 function sortear() {
+    if (sorteioRealizado) {
+        return alert('Sorteio realizado, não pode sortear novamente')
+    }
+
+    if (listaAmigosArray.length < 4) {
+        return alert('Adicione no minimo 4 participantes')
+    }
     embaralhar(listaAmigosArray)
 
     let sorteio = document.getElementById('lista-sorteio')
@@ -31,6 +47,7 @@ function sortear() {
            sorteio.innerHTML = sorteio.innerHTML + listaAmigosArray[i] +' --> ' +listaAmigosArray[i + 1] + '<br/>'
         }
     }
+    sorteioRealizado = true
 }
 
 function embaralhar(lista) {
@@ -41,6 +58,7 @@ function embaralhar(lista) {
 }
 
 function reiniciar() {
+    btnSortear.disabled = false
     listaAmigosArray = []
     document.getElementById('lista-amigos').innerHTML = ''
     document.getElementById('lista-sorteio').innerHTML = ''
