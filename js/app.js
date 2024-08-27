f116b94baccecb6932a976f1288cc8843c8fddfb
Giovani Silva <giovanibrolyy2016@gmail.com>
2024-08-26 21:55:18 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,43 @@
+let listaAmigosArray = []
+
+function adicionar() {
+    let nomeAmigo = document.getElementById('nome-amigo')
+    let listaAmigos = document.getElementById('lista-amigos')
+    
+    listaAmigosArray.push(nomeAmigo.value)
+
+    if (listaAmigos.textContent == '') {
+        listaAmigos.textContent = nomeAmigo.value
+    } else {
+        listaAmigos.textContent = listaAmigos.textContent + ', ' + nomeAmigo.value
+    }
+
+    nomeAmigo.value = ''
+}
+
+function sortear() {
+    embaralhar(listaAmigosArray)
+
+    let sorteio = document.getElementById('lista-sorteio')
+    for (let i = 0; i < listaAmigosArray.length; i++) {
+        if (i == listaAmigosArray.length - 1) {
+           sorteio.innerHTML = sorteio.innerHTML + listaAmigosArray[i] +' --> ' +listaAmigosArray[0] + '<br/>'
+        } else {
+           sorteio.innerHTML = sorteio.innerHTML + listaAmigosArray[i] +' --> ' +listaAmigosArray[i + 1] + '<br/>'
+        }
+    }
+}
+
+function embaralhar(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]]
+    }
+}
+
+function reiniciar() {
+    listaAmigosArray = []
+    document.getElementById('lista-amigos').innerHTML = ''
+    document.getElementById('lista-sorteio').innerHTML = ''
+
+}
